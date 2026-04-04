c82bac9ab326ef0ce79e159ab885fbc016cf7b03
mahmoud raafat <mahmoud.a.raafat05@email.com>
2026-04-04 17:56:38 +0200

    added first two users info

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <string>
-#include "All structures.h"
+#include "all_structures.h"
+#include <fstream>
 using namespace std;
 
-user users[10];
+user users[10] = {
+    {1,"hana","hana_123","hana@gmail.com","new cairo","01111567894", //user's info
+        {{"12223","hana morsy","455","03/27","CIB",8008.5}}, // account info
+        {{"hana","mahmoud",500}} //transactions 
+    },
+     {2,"mahmoud","mahmoud@pass","mahmoud@gmail.com","new nozha","01114563879", //user's info
+        {{"12224","mahmoud medhat","320","07/26","bank masr",405060.33}}, // account info
+        {{"mahmoud","hana",10000}} //transactions 
+    }
+};
+
 
 int main(){
    
+    return 0;
 }
