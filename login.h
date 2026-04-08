d9767d543ccd8d66bf287bea29c95f78bef609a6
yasmenamr557 <120678340+yasmenamr557@users.noreply.github.com>
2026-04-08 14:27:43 +0200

    final one

--- login.h
+++ login.h
@@ -5,23 +5,26 @@ using namespace std;
 void login() {
 	string username, password;
 	cout << "please enter your username: ";
+	cin.ignore();
 	getline(cin, username); // to read the username with spaces
+
 	cout << "please enter your password: ";
 
 	cin >> password;
-	
+
 	bool found = false; // to check if user is found
 
 	for (int i = 0; i < 10; i++) {
-		
+
 		if (users[i].username == username && users[i].password == password) {
 			found = true;
-			cout << "Login successful! Welcome, " << users[i].name << "." << endl;
+			cout << "Login successful! Welcome, " << users[i].username << "." << endl;
 
 			break;
 		}
-		if(!found)
-			cout << "Invalid username or password. Please try again." << endl;
-		
-	
+
+
 	}
+	if (!found)
+		cout << "Invalid username or password. Please try again." << endl;
+}
